449dbef6fbd1cd49f4b9d8cd90ad0e986cb6277f
Arjun KalyanRam <30958142+weirdoshere@users.noreply.github.com>
2017-11-05 22:38:27 +0530

    Add files via upload

---
+++ BoardConfigCommon.mk
@@ -0,0 +1,161 @@
+#
+# Copyright (C) 2014 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# inherit from the proprietary version
+-include vendor/motorola/msm8960dt-common/BoardConfigVendor.mk
+
+LOCAL_PATH := device/motorola/msm8960dt-common
+
+BOARD_VENDOR := motorola-qcom
+
+TARGET_SPECIFIC_HEADER_PATH := $(LOCAL_PATH)/include
+
+# Platform
+TARGET_BOARD_PLATFORM       := msm8960
+TARGET_BOARD_PLATFORM_GPU   := qcom-adreno320
+
+# Bootloader
+TARGET_BOOTLOADER_BOARD_NAME    := MSM8960
+TARGET_NO_BOOTLOADER            := true
+
+# Architecture
+TARGET_ARCH                             := arm
+TARGET_ARCH_VARIANT                     := armv7-a-neon
+TARGET_CPU_ABI                          := armeabi-v7a
+TARGET_CPU_ABI2                         := armeabi
+TARGET_CPU_SMP 							:= true
+TARGET_CPU_VARIANT                      := krait
+TARGET_USE_QCOM_BIONIC_OPTIMIZATION     := true
+TARGET_GLOBAL_CFLAGS 					+= -mfpu=neon -mfloat-abi=softfp
+TARGET_GLOBAL_CPPFLAGS 					+= -mfpu=neon -mfloat-abi=softfp
+
+# Kernel
+BOARD_CUSTOM_BOOTIMG_MK    := $(LOCAL_PATH)/mkbootimg.mk
+BOARD_KERNEL_LZ4C_DT 	   := true
+LZMA_RAMDISK_TARGETS 	   := recovery
+BOARD_KERNEL_CMDLINE       := console=ttyHSL0,115200,n8 androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3 maxcpus=2 androidboot.write_protect=0
+BOARD_KERNEL_CMDLINE       += androidboot.selinux=permissive
+BOARD_KERNEL_BASE          := 0x80200000
+BOARD_KERNEL_PAGESIZE      := 2048
+BOARD_KERNEL_SEPARATED_DT  := true
+BOARD_MKBOOTIMG_ARGS       := --ramdisk_offset 0x02200000
+TARGET_KERNEL_SOURCE       := kernel/motorola/ghost
+TARGET_KERNEL_CONFIG       := msm8960dt_mmi_defconfig
+
+# Audio
+BOARD_USES_ALSA_AUDIO               := true
+USE_CUSTOM_AUDIO_POLICY             := 1
+BOARD_USES_FLUENCE_INCALL           := true
+BOARD_USES_SEPERATED_AUDIO_INPUT    := true
+BOARD_USES_SEPERATED_VOICE_SPEAKER  := true
+AUDIO_USE_LL_AS_PRIMARY_OUTPUT 	    := true
+QCOM_MULTI_VOICE_SESSION_ENABLED    := true
+
+# Bluetooth
+BOARD_HAVE_BLUETOOTH        := true
+BOARD_HAVE_BLUETOOTH_QCOM   := true
+BLUETOOTH_HCI_USE_MCT       := true
+
+# Camera
+BOARD_GLOBAL_CFLAGS                     += -DCAMCORDER_GRALLOC_SOURCE
+BOARD_GLOBAL_CFLAGS 			+= -DMETADATA_CAMERA_SOURCE
+USE_DEVICE_SPECIFIC_CAMERA  		:= true
+TARGET_HAS_LEGACY_CAMERA_HAL1 		:= true
+TARGET_NEEDS_METADATA_CAMERA_SOURCE	:= true
+TARGET_NEEDS_GCC_LIBC 			:= true
+TARGET_CAMERASERVICE_CLOSES_NATIVE_HANDLES := true
+
+# Display
+NUM_FRAMEBUFFER_SURFACE_BUFFERS     := 3
+TARGET_DISPLAY_USE_RETIRE_FENCE     := true
+TARGET_USES_C2D_COMPOSITION         := true
+TARGET_USES_ION                     := true
+USE_OPENGL_RENDERER                 := true
+
+# Flags
+BOARD_USES_LEGACY_MMAP := true
+
+# Power HAL
+TARGET_POWERHAL_VARIANT := qcom
+
+# Qualcomm support
+BOARD_USES_QCOM_HARDWARE := true
+
+# Recovery
+TARGET_RECOVERY_FSTAB                   := $(LOCAL_PATH)/rootdir/etc/fstab.qcom
+TARGET_USERIMAGES_USE_EXT4              := true
+TARGET_USERIMAGES_USE_F2FS              := true
+BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE       := ext4
+TARGET_RECOVERY_DENSITY                 := xhdpi
+
+# RIL
+BOARD_RIL_CLASS := ../../../$(LOCAL_PATH)/ril/
+
+BOARD_SECCOMP_POLICY := device/motorola/msm8960dt-common/seccomp
+
+# SELinux
+-include device/qcom/sepolicy/sepolicy.mk
+
+BOARD_SEPOLICY_DIRS += \
+    $(LOCAL_PATH)/sepolicy
+
+# Wifi
+BOARD_HAS_QCOM_WLAN                 := true
+BOARD_HAS_QCOM_WLAN_SDK             := true
+BOARD_HOSTAPD_DRIVER                := NL80211
+BOARD_HOSTAPD_PRIVATE_LIB           := lib_driver_cmd_qcwcn
+BOARD_WLAN_DEVICE                   := qcwcn
+BOARD_WPA_SUPPLICANT_DRIVER         := NL80211
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB    := lib_driver_cmd_qcwcn
+WIFI_DRIVER_FW_PATH_AP              := "ap"
+WIFI_DRIVER_FW_PATH_STA             := "sta"
+WPA_SUPPLICANT_VERSION              := VER_0_8_X
+
+#TWRP
+# Recovery: TWRP support
+ifeq ($(WITH_TWRP),true)
+-include $(PLATFORM_PATH)/twrp.mk
+endif
+BOARD_GLOBAL_CFLAGS += -DNO_SECURE_DISCARD
+HAVE_SELINUX 						:= true
+BOARD_HAS_NO_REAL_SDCARD            := true
+RECOVERY_SDCARD_ON_DATA             := true
+TARGET_RECOVERY_PIXEL_FORMAT        := "RGB_565"
+TW_EXTERNAL_STORAGE_PATH            := "/usb-otg"
+TW_EXTERNAL_STORAGE_MOUNT_POINT     := "usb-otg"
+DEVICE_RESOLUTION                   := 720x1280
+RECOVERY_GRAPHICS_USE_LINELENGTH    := true
+BOARD_SUPPRESS_SECURE_ERASE         := true
+TW_INCLUDE_CRYPTO 			    	:= true
+TW_INCLUDE_L_CRYPTO 			    := true
+TW_CRYPTO_REAL_BLKDEV 				:= "/dev/block/platform/msm_sdcc.1/by-name/userdata"
+TW_FLASH_FROM_STORAGE 				:= true
+TARGET_HW_DISK_ENCRYPTION 		    := true
+TW_TARGET_USES_QCOM_BSP             := true
+TW_DEFAULT_EXTERNAL_STORAGE         := true
+TW_EXCLUDE_SUPERSU                  := true
+TW_INCLUDE_NTFS_3G                  := true
+TW_INTERNAL_STORAGE_MOUNT_POINT     := "data"
+TW_INTERNAL_STORAGE_PATH            := "/data/media"
+TW_NO_SCREEN_BLANK                  := true
+TW_NO_USB_STORAGE                   := true
+TW_INCLUDE_FUSE_EXFAT 				:= true
+#TW_THEME                            := portrait_hdpi
+TW_THEME                            := portrait_hdpi
+TW_BRIGHTNESS_PATH 				    := /sys/class/backlight/lcd-backlight/brightness
+TARGET_USE_CUSTOM_LUN_FILE_PATH     := /sys/devices/platform/msm_hsusb/gadget/lun0/file
+# Debug flags
+TWRP_INCLUDE_LOGCAT					:= true
+TW_MAX_BRIGHTNESS 					:= 126
---
+++ msm8960dt-common.mk
@@ -0,0 +1,220 @@
+#
+# Copyright (C) 2014 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+$(call inherit-product-if-exists, vendor/motorola/msm8960dt-common/msm8960dt-common-vendor.mk)
+
+# Overlay
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+
+# Permissions
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.audio.low_latency.xml:system/etc/permissions/android.hardware.audio.low_latency.xml \
+    frameworks/native/data/etc/android.hardware.bluetooth_le.xml:system/etc/permissions/android.hardware.bluetooth_le.xml \
+    frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
+    frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
+    frameworks/native/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
+    frameworks/native/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml \
+    frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:system/etc/permissions/android.hardware.sensor.accelerometer.xml \
+    frameworks/native/data/etc/android.hardware.sensor.barometer.xml:system/etc/permissions/android.hardware.sensor.barometer.xml \
+    frameworks/native/data/etc/android.hardware.sensor.compass.xml:system/etc/permissions/android.hardware.sensor.compass.xml \
+    frameworks/native/data/etc/android.hardware.sensor.gyroscope.xml:system/etc/permissions/android.hardware.sensor.gyroscope.xml \
+    frameworks/native/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
+    frameworks/native/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
+    frameworks/native/data/etc/android.hardware.sensor.stepcounter.xml:system/etc/permissions/android.hardware.sensor.stepcounter.xml \
+    frameworks/native/data/etc/android.hardware.sensor.stepdetector.xml:system/etc/permissions/android.hardware.sensor.stepdetector.xml \
+    frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
+    frameworks/native/data/etc/android.hardware.telephony.cdma.xml:system/etc/permissions/android.hardware.telephony.cdma.xml \
+    frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml \
+    frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
+    frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml \
+    frameworks/native/data/etc/android.hardware.wifi.direct.xml:system/etc/permissions/android.hardware.wifi.direct.xml \
+    frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
+    frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
+    frameworks/native/data/etc/com.android.nfc_extras.xml:system/etc/permissions/com.android.nfc_extras.xml \
+    frameworks/native/data/etc/com.nxp.mifare.xml:system/etc/permissions/com.nxp.mifare.xml \
+    frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml
+
+# System Properties
+include $(LOCAL_PATH)/system_prop.mk
+
+# Screen density
+PRODUCT_AAPT_CONFIG := normal
+PRODUCT_AAPT_PREF_CONFIG := xhdpi
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1280
+TARGET_SCREEN_WIDTH := 720
+
+$(call inherit-product, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
+
+# Audio
+PRODUCT_PACKAGES += \
+    audio.a2dp.default \
+    audio.primary.msm8960 \
+    audio.r_submix.default \
+    audio.usb.default \
+    libaudio-resampler \
+	libqcomvoiceprocessing \
+    tinymix
+
+# Audio config
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/audio_effects.conf:system/vendor/etc/audio_effects.conf \
+    $(LOCAL_PATH)/configs/audio_policy.conf:system/etc/audio_policy.conf \
+    $(LOCAL_PATH)/configs/audio_platform_info.xml:system/etc/audio_platform_info.xml \
+    $(LOCAL_PATH)/configs/mixer_paths.xml:system/etc/mixer_paths.xml
+
+
+PRODUCT_PACKAGES += \
+    mbhc.bin \
+    wcd9310_anc.bin
+
+# Android_filesystem_config.h
+PRODUCT_PACKAGES += \
+    fs_config_files
+
+# Camera
+PRODUCT_PACKAGES += \
+    camera.msm8960 \
+    Snap
+
+# Compat
+PRODUCT_PACKAGES += \
+    libboringssl-compat \
+    libstlport
+
+# Camerashims and others shims
+PRODUCT_PACKAGES += \
+    libshim_camera \
+    libshims_cameraservice \
+    libshim_log \
+    libshim_mdmcutback \
+    libshim_qcopt \
+    libshim_ril \
+    libshims_thermal \
+    libshims_graphicbuffer
+
+# Display
+PRODUCT_PACKAGES += \
+    copybit.msm8960 \
+    gralloc.msm8960 \
+    hwcomposer.msm8960 \
+    libgenlock \
+    memtrack.msm8960
+
+# Doze mode
+PRODUCT_PACKAGES += \
+    libjni_motoSensor \
+    MotoDoze
+
+# IRSC
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/sec_config:system/etc/sec_config
+
+#TWRP
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/rootdir/etc/twrp.fstab:recovery/root/etc/twrp.fstab
+
+# Keystore
+PRODUCT_PACKAGES += \
+    keystore.msm8960
+
+# Media
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/media_profiles.xml:system/etc/media_profiles.xml \
+    $(LOCAL_PATH)/configs/media_codecs.xml:system/etc/media_codecs.xml \
+    $(LOCAL_PATH)/configs/media_codecs_performance.xml:system/etc/media_codecs_performance.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml
+
+# Motorola
+PRODUCT_PACKAGES += \
+    charge_only_mode
+
+# NFC
+PRODUCT_PACKAGES += \
+    libnfc \
+    libnfc_jni \
+    nfc.msm8960 \
+    Nfc \
+    Tag \
+    com.android.nfc_extras
+
+ifeq ($(TARGET_BUILD_VARIANT),user)
+    PRODUCT_COPY_FILES += $(LOCAL_PATH)/configs/nfcee_access.xml:system/etc/nfcee_access.xml
+else
+    PRODUCT_COPY_FILES += $(LOCAL_PATH)/configs/nfcee_access_debug.xml:system/etc/nfcee_access.xml
+endif
+
+# OMX
+PRODUCT_PACKAGES += \
+    libOmxCore \
+    libOmxVdec \
+    libOmxVenc \
+    libOmxAacEnc \
+    libOmxAmrEnc \
+    libOmxEvrcEnc \
+    libOmxQcelp13Enc \
+    libstagefrighthw
+
+# Power HAL
+PRODUCT_PACKAGES += \
+    power.msm8960
+
+# Ramdisk
+PRODUCT_PACKAGES += \
+    init.qcom.bt.sh \
+    init.qcom.wifi.sh
+
+PRODUCT_PACKAGES += \
+    fstab.qcom \
+    init.mmi.boot.sh \
+    init.mmi.touch.sh \
+    init.qcom.rc \
+    init.qcom.usb.rc \
+    init.qcom.power.rc \
+    init.recovery.qcom.rc \
+    ueventd.qcom.rc
+
+# Support
+PRODUCT_PACKAGES += \
+    libcurl \
+    libxml2
+
+# Telephony-ext
+PRODUCT_PACKAGES += telephony-ext
+PRODUCT_BOOT_JARS += telephony-ext
+
+# Wifi
+PRODUCT_PACKAGES += \
+    hostapd \
+    libQWiFiSoftApCfg \
+    wpa_supplicant \
+    wpa_supplicant.conf
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/hostapd_default.conf:system/etc/hostapd/hostapd_default.conf \
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/p2p_supplicant_overlay.conf:system/etc/wifi/p2p_supplicant_overlay.conf \
+    $(LOCAL_PATH)/configs/wpa_supplicant_overlay.conf:system/etc/wifi/wpa_supplicant_overlay.conf
+
+PRODUCT_COPY_FILES += \
+    kernel/motorola/ghost/drivers/staging/prima_mmi/firmware_bin/WCNSS_cfg.dat:system/etc/firmware/wlan/prima/WCNSS_cfg.dat \
+    kernel/motorola/ghost/drivers/staging/prima_mmi/firmware_bin/WCNSS_qcom_cfg.ini:system/etc/wifi/WCNSS_qcom_cfg.ini
